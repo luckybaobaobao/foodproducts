c79061bd17d79d66251ac45e7278493529c7f007
AWS <noreply-aws@amazon.com>
2021-09-14 16:55:26 +0000

    Initial commit made by AWS during repository creation.

---
+++ helloworld/application.py
@@ -0,0 +1,37 @@
+#!flask/bin/python
+import json
+from flask import Flask, Response
+
+application = Flask(__name__)
+
+@application.route('/', methods=['GET'])
+def get():
+    return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
+
+@application.route('/', methods=['POST'])
+def post():
+    return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
+
+if __name__ == '__main__':
+    default_port = "80"
+    default_host = "0.0.0.0"
+    parser = optparse.OptionParser()
+    parser.add_option("-H", "--host",
+                      help=f"Hostname of Flask app {default_host}.",
+                      default=default_host)
+
+    parser.add_option("-P", "--port",
+                      help=f"Port for Flask app {default_port}.",
+                      default=default_port)
+
+    parser.add_option("-d", "--debug",
+                      action="store_true", dest="debug",
+                      help=optparse.SUPPRESS_HELP)
+
+    options, _ = parser.parse_args()
+
+    application.run(
+        debug=options.debug,
+        host=options.host,
+        port=int(options.port)
+    )
